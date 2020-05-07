297c246f2fdd8278b261e089cb5664a52ef0541c
Brian Clozel <bclozel@pivotal.io>
2020-05-07 15:33:27 +0200

    Fix Concourse promotion pipeline
    This commit fixes the promotion Concourse pipeline to download the
    staged artifacts as we need to use the generated jar to release itself.

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -5,7 +5,8 @@ source $(dirname $0)/common.sh
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
-cp `find ./artifactory-repo -name "concourse-release-scripts-*.jar"` /concourse-release-scripts.jar
+released=$(find ./artifactory-repo -name "concourse-release-scripts-*.jar")
+cp $released /concourse-release-scripts.jar
 
 java -jar /concourse-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION > /dev/null || { exit 1; }
 
