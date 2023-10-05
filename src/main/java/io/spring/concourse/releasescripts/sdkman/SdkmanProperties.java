17267f9b7cf9702d49983d5f0d32f33d54a9ef48
Andy Wilkinson <wilkinsona@vmware.com>
2023-10-05 11:27:05 +0100

    Remove unused field from SdkmanProperties

--- src/main/java/io/spring/concourse/releasescripts/sdkman/SdkmanProperties.java
+++ src/main/java/io/spring/concourse/releasescripts/sdkman/SdkmanProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,8 +39,6 @@ public class SdkmanProperties {
 
 	@Pattern(regexp = "[a-z.]+:[a-z\\-]+:[^:]+(:[a-z.]+(:[a-z]+)?)?")
 	private String artifact;
-
-	private String hashtag;
 
 	public String getConsumerKey() {
 		return this.consumerKey;
